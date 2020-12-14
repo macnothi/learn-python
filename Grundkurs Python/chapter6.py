1370c0eabf9bfeab8030ac8a7fc82529402cc5b1
macnothi <macnothi@me.com>
2020-12-14 22:42:18 +0100

    chapter 6, W1..W3

--- Grundkurs Python/chapter6.py
+++ Grundkurs Python/chapter6.py
@@ -9,3 +9,21 @@ print(dt.strftime('%A, %d. %B %Y')) # Ausgabe Wochentag, Tag. Monat Jahr
 # iso Format
 dt = datetime.now().isoformat() # aktuelles Dateum+ Zeit in ISO Format 
 print(dt) # 2020-12-14T00:14:45.465216
+
+#W1: Geben sie das heutige Datum inklusive Wochentag aber ohne Jahreszahl aus
+locale.setlocale(locale.LC_ALL,'de_DE.UTF-8')
+dt = datetime.now()    # aktuelles Datum + Zeit lesen
+print(dt.strftime('%A, %d. %B')) # Ausgabe Wochentag, Tag. Monat Jahr 
+
+#W2: Ein Kinofilm beginnt um 19:30Uhr und dauert 132 Minuten. Wann ist dieser zu Ende?
+from datetime import datetime, timedelta
+startTime = datetime(1970, 1, 1, hour=19,minute=30) # nur mit time() lässt sich nicht rechnen
+endTime = startTime + timedelta(minutes=132)
+print(endTime.time())
+
+#W3: Wieviele Sekunden sind seit Mitternacht vergangen?
+from datetime import time, datetime, timedelta
+nowTime = datetime.now()
+secondsSinceMidnight = nowTime.hour * 60 * 60 + nowTime.minute * 60 + nowTime.second
+print(nowTime.time())
+print(secondsSinceMidnight)

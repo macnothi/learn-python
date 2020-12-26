1312abb07159f62537f25c7253fa56645f462bfd
macnothi <macnothi@me.com>
2020-12-26 16:45:31 +0100

    Satzzeichen als Liste

--- Grundkurs Python/chapter7.py
+++ Grundkurs Python/chapter7.py
@@ -65,6 +65,7 @@ for x in numDict:
 
 #W7: Ermitteln sie die maximale Wortlänge in der folgenden Zeichenkette
 lorem = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+
 '''
 # Lösung aus Buch
 # Zeichenkette aus Wörtern und Leerzeichen bilden (Zahlen, Satzzeichen etc. entfernen)
@@ -74,13 +75,16 @@ sortedWords = sorted(words,key=len,reverse=True)
 print('Maximale Wortlänge :',len(sortedWords[0]))
 '''
 
-# Jens' Variante mit for in ...
+# Jens' Variante mit for in ... nicht so elegant, aber einfach
+# man iteriere durch die Liste der Zeichen und zähle die Wortlänge, man merke sich das längste Wort
+lorem = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
 n=0
 longestWord=0
+satzZeichen = [' ', ',' ,'.', '!', '?', '\"', '\'']
 for x in list(lorem):
     n=n+1
-    if x == ' ' or x == ',' or x == '.' :
-        if n>longestWord:
+    if x in satzZeichen: 
+        if n > longestWord:
             longestWord=n-1 # eins zurück...
         n=0 
 print('Die maximale Wortlänge ist {0} Zeichen:'.format(longestWord))

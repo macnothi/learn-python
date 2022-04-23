dcda0b7a1fbd25e33d908d4a7dc12ccc62d09819
Jens Nothnagel <macnothi@me.com>
2022-04-23 16:08:13 +0200

    euler 1

---
+++ eulerproject-1.py
@@ -0,0 +1,19 @@
+# https://projecteuler.net/problem=1
+# 23.4.22 JNO
+# results sum35(10) == 23, sum35(1000) == 233168 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+#!/usr/bin/env python3
+
+def sum35(n):
+    sum = 0
+    for r in range(1,n): # range is 1 .. 999 by definition
+        if r % 3 == 0 or r % 5 == 0:  # check if r divides by 3 or 5 without rest
+            sum += r    # add up
+    return sum
+
+
+# return result if called as script
+if __name__ == '__main__':
+    print(sum35(1000))

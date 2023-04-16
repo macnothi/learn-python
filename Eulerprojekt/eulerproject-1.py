8437f149a353f44a97c74bd3180c51c0429acd05
macnothi <macnothi@me.com>
2023-04-16 21:50:49 +0200

    return

--- Eulerprojekt/eulerproject-1.py
+++ Eulerprojekt/eulerproject-1.py
@@ -1,7 +1,8 @@
 # https://projecteuler.net/problem=1
 # 23.4.22 JNO
 # results sum35(10) == 23, sum35(1000) == 233168 
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+# The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 #!/usr/bin/env python3
